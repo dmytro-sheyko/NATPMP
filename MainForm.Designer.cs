cc0907d2939272b5adac8a5710763d19127c83c7
Dmytro Sheyko <dmytro.sheyko@sheyko.dmytro>
2021-03-19 22:29:25 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace NATPMP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.groupGateway = new System.Windows.Forms.GroupBox();
+            this.comboGateway = new System.Windows.Forms.ComboBox();
+            this.groupLocalPort = new System.Windows.Forms.GroupBox();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.groupExternalPort = new System.Windows.Forms.GroupBox();
+            this.textExternalPort = new System.Windows.Forms.TextBox();
+            this.groupLifetime = new System.Windows.Forms.GroupBox();
+            this.textLifetime = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.groupExternalAddress = new System.Windows.Forms.GroupBox();
+            this.groupStatus = new System.Windows.Forms.GroupBox();
+            this.textExternalAddress = new System.Windows.Forms.TextBox();
+            this.textStatus = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.groupGateway.SuspendLayout();
+            this.groupLocalPort.SuspendLayout();
+            this.groupExternalPort.SuspendLayout();
+            this.groupLifetime.SuspendLayout();
+            this.groupExternalAddress.SuspendLayout();
+            this.groupStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Controls.Add(this.groupGateway, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.groupLocalPort, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.groupExternalPort, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.groupLifetime, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.buttonStart, 0, 3);
+            this.tableLayoutPanel.Controls.Add(this.buttonStop, 1, 3);
+            this.tableLayoutPanel.Controls.Add(this.groupExternalAddress, 0, 4);
+            this.tableLayoutPanel.Controls.Add(this.groupStatus, 0, 5);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 7;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(289, 323);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // groupGateway
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.groupGateway, 2);
+            this.groupGateway.Controls.Add(this.comboGateway);
+            this.groupGateway.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupGateway.Location = new System.Drawing.Point(3, 3);
+            this.groupGateway.Name = "groupGateway";
+            this.groupGateway.Size = new System.Drawing.Size(283, 50);
+            this.groupGateway.TabIndex = 7;
+            this.groupGateway.TabStop = false;
+            this.groupGateway.Text = "Gateway";
+            // 
+            // comboGateway
+            // 
+            this.comboGateway.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboGateway.FormattingEnabled = true;
+            this.comboGateway.Location = new System.Drawing.Point(3, 16);
+            this.comboGateway.Name = "comboGateway";
+            this.comboGateway.Size = new System.Drawing.Size(277, 21);
+            this.comboGateway.TabIndex = 0;
+            // 
+            // groupLocalPort
+            // 
+            this.groupLocalPort.Controls.Add(this.textLocalPort);
+            this.groupLocalPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupLocalPort.Location = new System.Drawing.Point(3, 59);
+            this.groupLocalPort.Name = "groupLocalPort";
+            this.groupLocalPort.Size = new System.Drawing.Size(138, 50);
+            this.groupLocalPort.TabIndex = 0;
+            this.groupLocalPort.TabStop = false;
+            this.groupLocalPort.Text = "Local Port";
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textLocalPort.Location = new System.Drawing.Point(3, 16);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(132, 20);
+            this.textLocalPort.TabIndex = 0;
+            // 
+            // groupExternalPort
+            // 
+            this.groupExternalPort.Controls.Add(this.textExternalPort);
+            this.groupExternalPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupExternalPort.Location = new System.Drawing.Point(147, 59);
+            this.groupExternalPort.Name = "groupExternalPort";
+            this.groupExternalPort.Size = new System.Drawing.Size(139, 50);
+            this.groupExternalPort.TabIndex = 1;
+            this.groupExternalPort.TabStop = false;
+            this.groupExternalPort.Text = "Desired External Port";
+            // 
+            // textExternalPort
+            // 
+            this.textExternalPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textExternalPort.Location = new System.Drawing.Point(3, 16);
+            this.textExternalPort.Name = "textExternalPort";
+            this.textExternalPort.Size = new System.Drawing.Size(133, 20);
+            this.textExternalPort.TabIndex = 0;
+            // 
+            // groupLifetime
+            // 
+            this.groupLifetime.Controls.Add(this.textLifetime);
+            this.groupLifetime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupLifetime.Location = new System.Drawing.Point(3, 115);
+            this.groupLifetime.Name = "groupLifetime";
+            this.groupLifetime.Size = new System.Drawing.Size(138, 50);
+            this.groupLifetime.TabIndex = 2;
+            this.groupLifetime.TabStop = false;
+            this.groupLifetime.Text = "Lifetime (in seconds)";
+            // 
+            // textLifetime
+            // 
+            this.textLifetime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textLifetime.Location = new System.Drawing.Point(3, 16);
+            this.textLifetime.Name = "textLifetime";
+            this.textLifetime.Size = new System.Drawing.Size(132, 20);
+            this.textLifetime.TabIndex = 0;
+            this.textLifetime.Text = "3600";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonStart.Location = new System.Drawing.Point(3, 171);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(147, 171);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // groupExternalAddress
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.groupExternalAddress, 2);
+            this.groupExternalAddress.Controls.Add(this.textExternalAddress);
+            this.groupExternalAddress.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupExternalAddress.Location = new System.Drawing.Point(3, 200);
+            this.groupExternalAddress.Name = "groupExternalAddress";
+            this.groupExternalAddress.Size = new System.Drawing.Size(283, 50);
+            this.groupExternalAddress.TabIndex = 5;
+            this.groupExternalAddress.TabStop = false;
+            this.groupExternalAddress.Text = "External Address";
+            // 
+            // groupStatus
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.groupStatus, 2);
+            this.groupStatus.Controls.Add(this.textStatus);
+            this.groupStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupStatus.Location = new System.Drawing.Point(3, 256);
+            this.groupStatus.Name = "groupStatus";
+            this.groupStatus.Size = new System.Drawing.Size(283, 50);
+            this.groupStatus.TabIndex = 6;
+            this.groupStatus.TabStop = false;
+            this.groupStatus.Text = "Status";
+            // 
+            // textExternalAddress
+            // 
+            this.textExternalAddress.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textExternalAddress.Location = new System.Drawing.Point(3, 16);
+            this.textExternalAddress.Name = "textExternalAddress";
+            this.textExternalAddress.ReadOnly = true;
+            this.textExternalAddress.Size = new System.Drawing.Size(277, 20);
+            this.textExternalAddress.TabIndex = 0;
+            // 
+            // textStatus
+            // 
+            this.textStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textStatus.Location = new System.Drawing.Point(3, 16);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.ReadOnly = true;
+            this.textStatus.Size = new System.Drawing.Size(277, 20);
+            this.textStatus.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 323);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NAT-PMP";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.groupGateway.ResumeLayout(false);
+            this.groupLocalPort.ResumeLayout(false);
+            this.groupLocalPort.PerformLayout();
+            this.groupExternalPort.ResumeLayout(false);
+            this.groupExternalPort.PerformLayout();
+            this.groupLifetime.ResumeLayout(false);
+            this.groupLifetime.PerformLayout();
+            this.groupExternalAddress.ResumeLayout(false);
+            this.groupExternalAddress.PerformLayout();
+            this.groupStatus.ResumeLayout(false);
+            this.groupStatus.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.GroupBox groupGateway;
+        private System.Windows.Forms.ComboBox comboGateway;
+        private System.Windows.Forms.GroupBox groupLocalPort;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.GroupBox groupExternalPort;
+        private System.Windows.Forms.TextBox textExternalPort;
+        private System.Windows.Forms.GroupBox groupLifetime;
+        private System.Windows.Forms.TextBox textLifetime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.GroupBox groupExternalAddress;
+        private System.Windows.Forms.GroupBox groupStatus;
+        private System.Windows.Forms.TextBox textExternalAddress;
+        private System.Windows.Forms.TextBox textStatus;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NATPMP
+{
+    public partial class MainForm : Form
+    {
+        private const int PORT_NAT_PMP = 5351;
+        private const byte VER = 0;
+        private const byte OP_ADR = 0;
+        private const byte OP_TCP = 2;
+        private const int TIMEOUT = 2000;
+        private const int R_SUCCESS = 0;
+        private const int R_UNSUPPORTED_VERSION = 1;
+        private const int R_REFUSED = 2;
+        private const int R_NETWORK_FAILURE = 3;
+        private const int R_OUT_OF_RESOURCES = 4;
+        private const int R_UNSUPPORTED_OPCODE = 5;
+
+        private readonly UdpClient udp_;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            udp_ = new UdpClient(0, AddressFamily.InterNetwork);
+            udp_.Client.ReceiveTimeout = TIMEOUT;
+            Debug.WriteLine($"{udp_.Client.LocalEndPoint}");
+        }
+
+        private async void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (buttonStop.Enabled)
+            {
+                try
+                {
+                    IPAddress gatewayIP = IPAddress.Parse(comboGateway.Text.Trim());
+                    int portLocal = ParsePort(textLocalPort.Text.Trim());
+                    await RequestMapping(udp_, new IPEndPoint(gatewayIP, PORT_NAT_PMP), portLocal, 0, 0);
+                }
+                catch (Exception x)
+                {
+                    Debug.WriteLine(x);
+                }
+            }
+            udp_.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            IList<IPAddress> candidates = GetGatewayCandidates();
+            foreach (var item in candidates)
+            {
+                Debug.WriteLine($"{item}");
+                comboGateway.Items.Add(item);
+            }
+            if (candidates.Count > 0)
+            {
+                comboGateway.SelectedIndex = 0;
+            }
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            textExternalAddress.Text = "";
+            textStatus.Text = "";
+            buttonStart.Enabled = false;
+            string externalFullAddress = null;
+            string status = "OK";
+            bool mapped = false;
+            int mappedPort = 0;
+            try
+            {
+                IPAddress gatewayIP = IPAddress.Parse(comboGateway.Text.Trim());
+                int portLocal = ParsePort(textLocalPort.Text.Trim());
+                int portDesired = ParsePort(textExternalPort.Text.Trim());
+                int lifetime = int.Parse(textLifetime.Text.Trim());
+                Tuple<IPEndPoint, IPAddress> result0 = await RequestExternalIPAddress(udp_, new IPEndPoint(gatewayIP, PORT_NAT_PMP));
+                externalFullAddress = "" + result0.Item2;
+                if (portLocal != 0)
+                {
+                    mappedPort = await RequestMapping(udp_, result0.Item1, portLocal, portDesired, lifetime);
+                    externalFullAddress += ":" + mappedPort;
+                    mapped = true;
+                }
+            }
+            catch (Exception x)
+            {
+                status = x.Message;
+            }
+            Invoke((MethodInvoker)(() =>
+            {
+                textExternalAddress.Text = externalFullAddress;
+                textStatus.Text = status;
+                if (mapped)
+                {
+                    buttonStop.Enabled = true;
+                    comboGateway.Enabled = false;
+                    textLocalPort.Enabled = false;
+                    textExternalPort.Enabled = false;
+                    textLifetime.Enabled = false;
+                    textExternalPort.Text = "" + mappedPort;
+                }
+                else
+                {
+                    buttonStart.Enabled = true;
+                }
+            }));
+        }
+
+        private async void buttonStop_Click(object sender, EventArgs e)
+        {
+            textExternalAddress.Text = "";
+            textStatus.Text = "";
+            buttonStop.Enabled = false;
+            string status = "OK";
+            bool mapped = true;
+            try
+            {
+                IPAddress gatewayIP = IPAddress.Parse(comboGateway.Text.Trim());
+                int portLocal = ParsePort(textLocalPort.Text.Trim());
+                int portDesired = ParsePort(textExternalPort.Text.Trim());
+                await RequestMapping(udp_, new IPEndPoint(gatewayIP, PORT_NAT_PMP), portLocal, portDesired, 0);
+                mapped = false;
+            }
+            catch (Exception x)
+            {
+                status = x.Message;
+            }
+            Invoke((MethodInvoker)(() =>
+            {
+                textStatus.Text = status;
+                if (mapped)
+                {
+                    buttonStop.Enabled = true;
+                }
+                else
+                {
+                    buttonStart.Enabled = true;
+                    comboGateway.Enabled = true;
+                    textLocalPort.Enabled = true;
+                    textExternalPort.Enabled = true;
+                    textLifetime.Enabled = true;
+                    textExternalAddress.Text = "";
+                }
+            }));
+        }
+
+        static async Task<Tuple<IPEndPoint, IPAddress>> RequestExternalIPAddress(UdpClient udp, IPEndPoint gateway)
+        {
+            byte[] data = new byte[] { VER, OP_ADR, };
+            Debug.WriteLine($"OUTGOING: {gateway} {ByteArrayToString(data)}");
+            udp.Send(data, data.Length, gateway);
+            return await Task.Run(() =>
+            {
+                IPEndPoint peer = null;
+                byte[] incoming;
+                try
+                {
+                    incoming = udp.Receive(ref peer);
+                    Debug.WriteLine($"INCOMING: {peer} {ByteArrayToString(incoming)}");
+                }
+                catch (Exception x)
+                {
+                    Debug.WriteLine($"{x}");
+                    throw;
+                }
+                if (incoming.Length == 12)
+                {
+                    CheckResult(incoming);
+                    IPAddress addr = new IPAddress(new byte[] { incoming[8], incoming[9], incoming[10], incoming[11] });
+                    return Tuple.Create(peer, addr);
+                }
+                else
+                {
+                    throw new Exception($"incoming.Length = {incoming.Length}, expected 12");
+                }
+            });
+        }
+
+        static async Task<int> RequestMapping(UdpClient udp, IPEndPoint gateway, int localPort, int desiredPort, int lifetime)
+        {
+            byte[] data = new byte[] {
+                VER, OP_TCP, 0, 0,
+                (byte) (localPort >> 8), (byte) localPort,
+                (byte) (desiredPort >> 8), (byte) desiredPort,
+                (byte) (lifetime >> 24), (byte) (lifetime >> 16), (byte) (lifetime >> 8), (byte) lifetime,
+            };
+            Debug.WriteLine($"OUTGOING: {gateway} {ByteArrayToString(data)}");
+            udp.Send(data, data.Length, gateway);
+            return await Task.Run(() =>
+            {
+                IPEndPoint peer = null;
+                byte[] incoming;
+                try
+                {
+                    incoming = udp.Receive(ref peer);
+                    Debug.WriteLine($"INCOMING: {peer} {ByteArrayToString(incoming)}");
+                }
+                catch (Exception x)
+                {
+                    Debug.WriteLine($"{x}");
+                    throw;
+                }
+                if (incoming.Length == 16)
+                {
+                    CheckResult(incoming);
+                    int mappedPort = (incoming[10] & 0xff) << 8 | (incoming[11] & 0xff);
+                    return mappedPort;
+                }
+                else
+                {
+                    throw new Exception($"incoming.Length = {incoming.Length}, expected 16");
+                }
+            });
+        }
+
+
+        static void CheckResult(byte[] incoming)
+        {
+            int res = ((incoming[2] & 0xff) << 8) | ((incoming[3] & 0xff));
+            CheckResult(res);
+        }
+
+        static void CheckResult(int res)
+        {
+            switch (res)
+            {
+                case R_SUCCESS:
+                    return;
+                case R_UNSUPPORTED_VERSION:
+                    throw new Exception($"{res}: Unsupported Version");
+                case R_REFUSED:
+                    throw new Exception($"{res}: Not Authorized/Refused");
+                case R_NETWORK_FAILURE:
+                    throw new Exception($"{res}: Network Failure");
+                case R_OUT_OF_RESOURCES:
+                    throw new Exception($"{res}: Out of resources");
+                case R_UNSUPPORTED_OPCODE:
+                    throw new Exception($"{res}: Unsupported opcode");
+                default:
+                    throw new Exception($"{res}: ???");
+            }
+        }
+
+        private static int ParsePort(string portText)
+        {
+            int port = 0;
+            if (portText.Length > 0)
+            {
+                port = int.Parse(portText);
+                if (port > IPEndPoint.MaxPort)
+                {
+                    throw new ArgumentOutOfRangeException("port", portText, $"port={port} is greater than {IPEndPoint.MaxPort}");
+                }
+                else if (port < IPEndPoint.MinPort)
+                {
+                    throw new ArgumentOutOfRangeException("port", portText, $"port={port} is less than {IPEndPoint.MinPort}");
+                }
+            }
+            return port;
+        }
+
+        static IList<IPAddress> GetGatewayCandidates()
+        {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+                .Select(g => g?.Address)
+                .Where(a => a != null)
+                .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
+                .ToList();
+        }
+        static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat(" {0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
